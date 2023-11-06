c14097a274ef4d7fc2c85130fda059667b2b19b0
Ruan Delgado Gomes <ruandgomes@gmail.com>
2023-11-06 09:54:33 -0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -2,14 +2,23 @@
 
 import os
 import sys
-import time
+import argparse
 
 from communicator.communicator import Communicator
 
-time.tzset()
 
-proxyAddr = "0.0.0.0"
-proxyPort = 50000
+# Construct the argument parser
+ap = argparse.ArgumentParser()
+
+# Add the arguments to the parser
+ap.add_argument("-a", "--address", required=True,
+   help="IP Address or URL")
+ap.add_argument("-p", "--port", required=True,
+   help="Port")
+args = vars(ap.parse_args())
+
+proxyAddr = args['address']
+proxyPort = int(args['port'])
 
 verbose = True
 
